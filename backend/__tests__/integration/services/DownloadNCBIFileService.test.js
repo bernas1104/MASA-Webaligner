067628c7b3a3fca13ac58335a71c1737a27f2570
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-04-27 19:07:17 -0300

    Starting on FetchBinaryServices.test.js

---
+++ backend/__tests__/integration/services/DownloadNCBIFileService.test.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const mz = require('mz/fs');
+const { uuid } = require('uuidv4');
+
+const AppError = require('../../../src/errors/AppError');
+
+const DownloadNCBIFileService = require(
+    '../../../src/services/DownloadNCBIFileService'
+);
+
+describe('Service responsible for accessing the NCBI API and retrieve FASTA files', () => {
+    const downloadNCBIFileService = new DownloadNCBIFileService();
+
+    const id = uuid();
+    const sequence = 'AF133821.1';
+
+    it('should download a fasta sequence file', async () => {
+        const filePath = path.resolve(__dirname, '..', '..', 'uploads');
+
+        const name = await downloadNCBIFileService.execute({ id, sequence });
+
+        const check = await mz.exists(path.join(filePath), name);
+
+        expect(check).toBe(true);
+    });
+
+    it('should download a file if the \'sequence\' is an empty string', async () => {
+        const filePath = path.resolve(__dirname, '..', '..', 'uploads');
+
+        const name = await downloadNCBIFileService.execute({ id, sequence: '' });
+
+        const check = await mz.exists(path.join(filePath), name);
+
+        expect(check).toBe(true);
+    });
+
+    it('should throw an error if the sequence name does not exist', async () => {
+        const filePath = path.resolve(__dirname, '..', '..', 'uploads');
+
+        try {
+            await downloadNCBIFileService.execute({
+                id,
+                sequence: 'af1338211.5'
+            });
+        } catch (err) {
+            expect(err instanceof AppError).toBe(true);
+        }
+    });
+});
