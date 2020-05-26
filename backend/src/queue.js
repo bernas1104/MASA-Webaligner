77181eb2653eeb31da547f728ed26283d6cd0b83
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-05-26 10:42:38 -0300

    JavaScript versions deleted

--- backend/src/queue.js
+++
@@ -1,8 +0,0 @@
-require('dotenv/config');
-
-const Queue = require('./lib/Queue');
-
-const { masaQueue, mailQueue } = Queue;
-
-masaQueue.bull.process(masaQueue.handle);
-mailQueue.bull.process(mailQueue.handle);
--- backend/src/services/CheckFastaFormatService.js
+++
@@ -1,8 +0,0 @@
-class CheckFastaFormatService {
-  execute({ sequence }) {
-    const verifier = /(>[A-Z]+_?[0-9]{5,6}\.[0-9]{1} (.*)\n)?([AGCTN]+[\n]?)/gy;
-    return verifier.test(sequence);
-  }
-}
-
-module.exports = CheckFastaFormatService;
